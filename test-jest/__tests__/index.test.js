9526dc8dc8e7b7c78c1662e5930a3c7eeca8da14
Matthew Warman <warmanmr@gmail.com>
2021-12-09 14:08:22 -0500

    Updated to Node14; AWS SDK 3

--- test-jest/__tests__/index.test.js
+++ test-jest/__tests__/index.test.js
@@ -1,5 +1,29 @@
-const handler = require('../index');
+const { _hello, _root } = require('../index');
 
-test('should return greeting', () => {
-  expect(handler.getGreeting()).toBe('Hello World!');
+test('path / should return default greeting', () => {
+  const req = {};
+  const res = {
+    text: '',
+    send: function (input) { this.text = input }
+  };
+
+  _root(req, res);
+
+  expect(res.text).toBe('Hello world!');
+});
+
+test('path /hello/:name should return bespoke greeting', () => {
+  const req = {
+    params: {
+      name: 'LeanStacks'
+    }
+  };
+  const res = {
+    text: '',
+    send: function (input) { this.text = input }
+  };
+
+  _hello(req, res);
+
+  expect(res.text).toBe('Hello LeanStacks!');
 });
