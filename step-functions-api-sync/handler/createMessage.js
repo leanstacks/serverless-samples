716e1ee1be40971d0d976151ad91b7e6f25b287a
Matthew Warman <warmanmr@gmail.com>
2021-12-13 08:32:43 -0500

    Updated to Node 14, AWS SDK 3

--- step-functions-api-sync/handler/createMessage.js
+++ step-functions-api-sync/handler/createMessage.js
@@ -1,32 +1,41 @@
 const serverless = require('serverless-http');
-const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-app.use(bodyParser.json({ strict: false }));
+
+app.use(express.json({ strict: false }));
 
 const { startExecutionAndWait } = require('../stepf/execution');
 
 /**
- * The create message endpoint.
+ * Route: POST /messages
+ * A route handler function to create a message.
  */
 app.post('/messages', function (req, res) {
 
+  console.log('Route::createMessage');
+
   // validate request
   const { audience } = req.body;
-  if (typeof audience !== 'string') {
-    res.status(400).json({ message: '"audience" must be a string' });
-  }
+  if (!audience || typeof audience !== 'string') {
 
-  // perform business logic, i.e. invoke the step function state machine
-  startExecutionAndWait(req.body)
-    .then((output) => {
-      console.log(`Execution result.\n${JSON.stringify(output, null, 2)}`);
-      res.json(output);
-    })
-    .catch((err) => {
-      console.log(`Error occurred. Detail:\n${JSON.stringify(err, null, 2)}`);
-      res.status(500).json(err);
-    });
+    // request invalid
+    res.status(400).json({ message: '"audience" must be a string' });
+
+  } else {
+
+    // request valid
+    // perform business logic, i.e. invoke the step function state machine
+    startExecutionAndWait(req.body)
+      .then((output) => {
+        console.log(`Execution result.\n${JSON.stringify(output, null, 2)}`);
+        res.json(output);
+      })
+      .catch((err) => {
+        console.log('Error occurred. Detail: ', error);
+        res.status(500).json(err);
+      });
+
+  }
 
 });
 
--- step-functions-api-sync/handler/toUpperCase.js
+++ step-functions-api-sync/handler/toUpperCase.js
@@ -1,21 +1,23 @@
-
 /**
  * Function: To Uppercase
  * Description: Convert the message to UPPER CASE.
  * Stage: 2
  */
 exports.handle = async (event, context) => {
+
   console.log(`event:\n${JSON.stringify(event, null, 2)}`);
   console.log(`context:\n${JSON.stringify(context, null, 2)}`);
 
-  /* The result from the previous Step Function Stage is the event, i.e. the input, for this Stage. */
+  // the result from the previous step/stage is the event, i.e. the input, for this step/stage
   const result = {
     ...event
   };
 
+  // if the message attribute exists, convert it to UPPERCASE
   if (!!result.message) {
     result.message = result.message.toUpperCase();
   }
 
   return result;
+
 }
